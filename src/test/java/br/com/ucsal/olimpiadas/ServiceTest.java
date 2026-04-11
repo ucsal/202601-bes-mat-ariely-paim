50365d069ca4cfe0ee33f85d69daad5e94518ad7
Ariely Paim <arielyfloquet@gmail.com>
2026-04-11 18:26:23 -0300

    feat: mudança do nome da aplicação 'ExemploTeste.java' para 'ServiceTest.java'

--- src/test/java/br/com/ucsal/olimpiadas/ServiceTest.java
+++ src/test/java/br/com/ucsal/olimpiadas/ServiceTest.java
@@ -3,7 +3,7 @@ package br.com.ucsal.olimpiadas;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ExemploTest {
+public class ServiceTest {
 
     @Test
     void teste() {
