b2e07ecf046b477b49417d3a5e6d2f57628b458d
Ariely Paim <arielyfloquet@gmail.com>
2026-04-11 18:13:44 -0300

    feat: inserção das aplicações 'Repository"

--- src/main/java/br/com/ucsal/olimpiadas/repository/QuestaoRepository.java
+++ src/main/java/br/com/ucsal/olimpiadas/repository/QuestaoRepository.java
@@ -1,5 +1,29 @@
 package br.com.ucsal.olimpiadas.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.ucsal.olimpiadas.model.Questao;
+
 public class QuestaoRepository {
-    
+    private List<Questao> lista = new ArrayList<>();
+    private long proximoId = 1;
+
+    public Questao salvar(Questao questao) {
+        if (questao.getId() == 0) {
+            questao.setId(proximoId++);
+        }
+        lista.add(questao);
+        return questao;
+    }
+
+    public List<Questao> buscarPorProvaId(long provaId) {
+        List<Questao> resultado = new ArrayList<>();
+        for (Questao q : lista) {
+            if (q.getProvaId() == provaId) {
+                resultado.add(q);
+            }
+        }
+        return resultado;
+    }
 }
--- src/main/java/br/com/ucsal/olimpiadas/repository/TentativaRepository.java
+++ src/main/java/br/com/ucsal/olimpiadas/repository/TentativaRepository.java
@@ -1,5 +1,23 @@
 package br.com.ucsal.olimpiadas.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.ucsal.olimpiadas.model.Tentativa;
+
 public class TentativaRepository {
-    
+    private List<Tentativa> lista = new ArrayList<>();
+    private long proximoId = 1;
+
+    public Tentativa salvar(Tentativa tentativa) {
+        if (tentativa.getId() == 0) {
+            tentativa.setId(proximoId++);
+        }
+        lista.add(tentativa);
+        return tentativa;
+    }
+
+    public List<Tentativa> listarTodos() {
+        return lista;
+    }
 }
